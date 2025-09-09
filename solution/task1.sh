0bf45f5278de5bf83d25bf94dd7f14a698bb5ef9
Sorce <sorce6285@gmail.com>
2025-09-09 19:07:04 +0700

    task1.sh

--- solution/task1.sh
+++ solution/task1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 temp_dir=$(mktemp -d)
-tar -xf data/archive-part1.tar -C "$temp_dir"
-unzip -q data/archive-part2.zip -d "$temp_dir"
-tar -czf data/archive-combined.tar.gz -C "$temp_dir" .
-rm -rf "$temp_dir"
+tar -xf data/archive-part1.tar -C $temp_dir
+unzip -q data/archive-part2.zip -d $temp_dir
+tar -czf data/archive-combined.tar.gz -C $temp_dir .
+rm -rf $temp_dir
